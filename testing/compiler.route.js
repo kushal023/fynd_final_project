39133d4afa0c7aa05e4d3e7bff62dd6137c4b0bf
Kushal Chauhan <kushal2017328@gmail.com>
2021-10-21 22:04:42 +0530

    initial commit

--- testing/compiler.route.js
+++ testing/compiler.route.js
@@ -1,9 +1,10 @@
 const express=require("express")
 const router=express.Router()
+const authenticate=require("../user/user.authenticate")
 
 const   {code, result}=require("./compiler.controller")
 
-router.post("/run",code)
+router.post("/run",authenticate,code)
 router.get("/result", result)
 
 module.exports=router
--- user/user.controller.js
+++ user/user.controller.js
@@ -2,6 +2,8 @@ const express = require("express")
 const bcrypt = require("bcryptjs")
 const User = require("./user.model")
 const jwt = require("jsonwebtoken")
+const DB = require('../db');
+
 const Problem=require("../HR/question.model")
 const Job=require("../testing/compiler.model")
 
@@ -90,63 +92,24 @@ const login = (req, res, next) => {
 
 
 
-// //Get question
-//    //First load the user 
-//     /**'
-//      * User object will be of this format
-//      * {
-//      *  attempts: {
-//      *  level1: [{questionId, answer, isCorrect}]
-//      * }
-//      * }
-//      /
-//     //Second check till which level has he submitted answer. Lets say this is levelN
-//     //Third generate a query for questions where level=levelN and _id not in user.response[levelN].map((r) => r.questionId)
-//     //From this query find out the number of questions in database with count() function. Lets say count
-//     //Next, generate a random number between 0 and count - 1
-//     //Repeat same query as above to fetch only 1 question from offset randomNumber
-//     //Update the user in Mongo with this question also added to attempts. Add {questionId} into the attempts[levelN]
-//     //Send to the user
-//     /*
-// const showProblem=async (req,res, next)=>{
-//     const attemptedProblems = (getUser(req.token).attempts || []).map((a) => a.questionId)
-//     const totalUnseenProblems = await Problem.find({_id: {not_in: attemptedProblems}});
-//     const randomIndex = Math.floor(Math.random() * totalUnseenProblems);
-//     const nextQuestion = Problem.find({_id: {not_in: attemptedProblems}}, {_id:0, questionId: "$_id",question:1 }).limit(1).skip(randomIndex);
-//     user.attempts.push({questionId: nextQuestion._id});
+//Get question
+   //First load the user 
     
-//     // .catch(error=>{
-//     //     res.status(400).json({
-//     //         error
-//     //     })
-//     // })
+    //  User object will be of this format
+    //   {
+    //   attempts: {
+    //    level1: [{questionId, answer, isCorrect}]
+    //   }
+    //   }
 
-// }
+// within array of attempts for given questionid
 
-
-// const handleResponse = () => {
-//     const user = getUser(req.token);
-//     //Check if user has attempted this questionId
-//     const isSeen = user.attempts.find({questionId: req.questionId});
-//     if (!isSeen) {
-//         return error
-//     }
-//     const isCorrect = isAnswerCorrect();
-//     User.update({_id: user._id}, {$set: {}})
-// }
-//within array of attempts for given questionid
-
-// const isAnswerCorrect=(Problem, SubmittedAnswer)=>{
-   
-//     var problem = Problem.findOne({_id: SubmittedAnswer.questionId}, {_id:0,question:0,level:0})
-//     const output = getAnswer(SubmittedAnswer);
-//     return problem.answer === output;
-// }
 
 
 const showProblem=(req,res, next)=>{
-    Problem.find({}, {question:1, _id:0}).limit(1)
-    .then(response=>{
+ 
+   Problem.find({}, {question:1, _id:0,questionId:"$_id"}).limit(1).skip(Math.random()*10)
+   .then(response=>{
         res.json({
             response
         })
@@ -157,7 +120,35 @@ const showProblem=(req,res, next)=>{
         })
     })
 
+    const attemptedProblems = (getUser(req.token).attempts || []).map((a) => a.questionId)
+    const totalUnseenProblems = await Problem.find({_id: {not_in: attemptedProblems}});
+    const randomIndex = Math.floor(Math.random() * totalUnseenProblems);
+    const nextQuestion = Problem.find({_id: {not_in: attemptedProblems}}, {_id:0, questionId: "$_id",question:1 }).limit(1).skip(randomIndex);
+    user.attempts.push({questionId: nextQuestion._id});
 }
+
+
+const handleResponse = (req) => {
+
+    const user = getUser(req.token);
+    //Check if user has attempted this questionId
+    const isSeen = user.attempts.find({questionId: req.questionId});
+    if (!isSeen) {
+        return error
+    }
+    User.update({_id: user._id}, {$set: {}})
+}
+
+
+const isAnswerCorrect=(Problem, SubmittedAnswer)=>{
+   
+    var problem = Problem.findOne({_id: SubmittedAnswer.questionId}, {_id:0,question:0,level:0})
+    const output = getAnswer(SubmittedAnswer);
+    return problem.answer === output;
+}
+
+
+
 
 
 module.exports = {
